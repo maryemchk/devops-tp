7b8db27b2b7609de672ff08267bdddb461457ccf
Maryem <chakrounmeryem23@gmail.com>
2025-02-25 22:02:02 +0100

    Added /status route

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,2 +1,12 @@
 # app .py
-print ("Hello , CI/CD!")
+from flask import Flask
+app = Flask ( __name__ )
+@app . route ('/')
+def hello ():
+   return "Hello , CI/CD!"
+if __name__ == " __main__ ":
+   app . run ()
+
+@app.route('/status')
+def status():
+    return "API is running!"
